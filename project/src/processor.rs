14dde90c64879d1182ddf34cb25b0db7183bbe76
nomadic4life <orbison.dennis@gmail.com>
2024-02-12 07:50:33 -0800

    mapped the calc processes for each instruction, but doesn't deal with state yet

--- project/src/processor.rs
+++ project/src/processor.rs
@@ -1,124 +1,128 @@
 use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
     msg,
     program::{invoke, invoke_signed},
     program_error::ProgramError,
-    account_info::{next_account_info, AccountInfo},
-    entrypoint::ProgramResult,
-    pubkey::Pubkey
-}
+    pubkey::Pubkey,
+};
 
-use crate::{instruction::CalcInstruction}
+// use crate::{instruction::CalcInstruction};
+// use instruction::CalcInstruction;
+
+use crate::instruction::CalcInstruction;
 
 pub struct Processor;
 impl Processor {
     pub fn process(
-        program_id:         &Pubkey,
-        accounts:           &[AccountInfo],
-        instruction_data:   &[u8],
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
     ) -> ProgramResult {
-
         let instruction;
 
         match instruction {
+            CalcInstruction::InitState => {
+                msg!("Init Output Account");
 
-            CalcInstruction::InitCalculatorState { output } => {.
-
-                msg!("Init of Output Account");
-
-                return Self::process_init_output(accounts, output, program_id);
+                return Self::process_init_state(accounts, program_id);
             }
 
-            CalcInstruction::AddOperation { operandA, operandB } => {
+            CalcInstruction::LoadOperand { operand } => {
+                msg!("Clear and load data into state buffer: {}", operand);
 
-                msg!("OUTPUT: {}", output);
-
-                return Self::process_add_operation(accounts, operandA, operandB, program_id);
+                return Self::process_load_operand(accounts, operand, program_id);
             }
 
-            CalcInstruction::SubOperation { operandA, operandB } => {
+            CalcInstruction::Add { operand } => {
+                msg!("ADD: {}", operand);
 
-                msg!("OUTPUT: {}", output);
-
-                return Self::process_sub_operation(accounts, operandA, operandB, program_id);
+                return Self::process_add_operation(accounts, operand, program_id);
             }
 
-            CalcInstruction::MulOperation { operandA, operandB } => {
+            CalcInstruction::Sub { operand } => {
+                msg!("SUB: {}", operand);
 
-                msg!("OUTPUT: {}", output);
-
-                return Self::process_sub_operation(accounts, operandA, operandB, program_id);
+                return Self::process_sub_operation(accounts, operand, program_id);
             }
 
-            CalcInstruction::DivOperation { operandA, operandB } => {
+            CalcInstruction::Mul { operand } => {
+                msg!("MUL: {}", operand);
 
-                msg!("OUTPUT: {}", output);
-
-                return Self::process_sub_operation(accounts, operandA, operandB, program_id);
+                return Self::process_mul_operation(accounts, operand, program_id);
             }
 
+            CalcInstruction::Div { operand } => {
+                msg!("DIV: {}", operand);
+
+                return Self::process_div_operation(accounts, operand, program_id);
+            }
+
+            CalcInstruction::Mod { operand } => {
+                msg!("MOD: {}", operand);
+
+                return Self::process_mod_operation(accounts, operand, program_id);
+            }
         }
     }
 
-    fn process_init_output(
-        accounts:   &[AccountInfo],
-        output:     u64,
-        program_id: &Pubkey
-    ) -> ProgramResult {
+    fn process_init_state(accounts: &[AccountInfo], program_id: &Pubkey) -> ProgramResult {
+        return Ok(());
+    }
 
+    fn process_load_operand(
+        accounts: &[AccountInfo],
+        operand: u64,
+        program_id: &Pubkey,
+    ) -> ProgramResult {
+        return Ok(());
     }
 
     fn process_add_operation(
-        accounts:   &[AccountInfo],
-        operandA:   u64,
-        operandB:   u64,
-        program_id: &Pubkey
+        accounts: &[AccountInfo],
+        operand: u64,
+        program_id: &Pubkey,
     ) -> ProgramResult {
-
-        let result = operandA + operandB;
-        
+        return Ok(());
     }
 
     fn process_sub_operation(
-        accounts:   &[AccountInfo],
-        operandA:   u64,
-        operandB:   u64,
-        program_id: &Pubkey
+        accounts: &[AccountInfo],
+        operand: u64,
+        program_id: &Pubkey,
     ) -> ProgramResult {
-
-        let result = operandA - operandB;
-        
+        return Ok(());
     }
 
     fn process_mul_operation(
-        accounts:   &[AccountInfo],
-        operandA:   u64,
-        operandB:   u64,
-        program_id: &Pubkey
+        accounts: &[AccountInfo],
+        operand: u64,
+        program_id: &Pubkey,
     ) -> ProgramResult {
-        
-        let result = operandA * operandB;
-
+        return Ok(());
     }
 
     fn process_div_operation(
-        accounts:   &[AccountInfo],
-        operandA:   u64,
-        operandB:   u64,
-        program_id: &Pubkey
+        accounts: &[AccountInfo],
+        operand: u64,
+        program_id: &Pubkey,
     ) -> ProgramResult {
-        
-        let result = operandA / operandB;
-
+        return Ok(());
     }
 
     fn process_mod_operation(
-        accounts:   &[AccountInfo],
-        operandA:   u64,
-        operandB:   u64,
-        program_id: &Pubkey
+        accounts: &[AccountInfo],
+        operand: u64,
+        program_id: &Pubkey,
     ) -> ProgramResult {
-        
-        let result = operandA % operandB;
+        let account_info_iter = &mut accounts.iter();
 
+        return Ok(());
+    }
+
+    fn store(output: u64, program_id: &Pubkey) -> () {
+        let (pda, _bump) = Pubkey::find_program_addres(&[b"output_buffer"], program_id);
+
+        return Ok(());
     }
 }
